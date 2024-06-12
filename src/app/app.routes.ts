7d3e44beb65abaa9e386bc47aa72a1e8de7f40b0
A Venkatesh <avenkateshanand1998@gmail.com>
2024-06-12 14:42:54 +0530

    i have created Login functionality for this project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/admin/login/login.component';
+import { LayoutComponent } from './pages/admin/layout/layout.component';
+import { ProductsComponent } from './pages/admin/products/products.component';
 
 export const routes: Routes = [
 
@@ -11,5 +13,16 @@ export const routes: Routes = [
     {
         path:"login",
         component:LoginComponent
+    },
+
+    {
+        path:"",
+        component:LayoutComponent,
+        children:[
+            {
+                path:"products",
+                component:ProductsComponent
+            }
+        ]
     }
 ];
