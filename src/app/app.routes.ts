93af141d3ea292d940cb5bb8fbf01c080559121d
A Venkatesh <avenkateshanand1998@gmail.com>
2024-06-12 13:33:37 +0530

    i have created Login Page Template and added for styles also

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/admin/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:'',
+        redirectTo:"login",
+        pathMatch:"full"
+    },
+    {
+        path:"login",
+        component:LoginComponent
+    }
+];
